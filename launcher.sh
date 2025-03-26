734234a267635a355d078cd0d578618e6ae77fe5
jorgesuela <104921157+jorgesuela@users.noreply.github.com>
2025-03-26 21:06:33 +0100

    se ha cambiado el mapa de simulacion

--- launcher.sh
+++ launcher.sh
@@ -16,14 +16,12 @@ gnome-terminal -- bash -c "rosrun turtlebot_chatgpt_wrapper ChatGPTProcessorNode
 gnome-terminal -- bash -c "rosrun turtlebot_chatgpt_wrapper TurtleBotControlNode.py; exec bash"
 
 # Iniciar Gazebo (Simulador)
-gnome-terminal -- bash -c "roslaunch turtlebot3_gazebo turtlebot3_empty_world.launch; exec bash"
+gnome-terminal -- bash -c "roslaunch turtlebot3_gazebo turtlebot3_world.launch; exec bash"
 
-# Iniciar nodo 5: GMapping SLAM (Creación del mapa)
+# Iniciar nodo 5: (Solo si estás utilizando SLAM)
 #gnome-terminal -- bash -c "roslaunch turtlebot3_slam turtlebot3_slam.launch; exec bash"
 
-# Esperar a que SLAM se inicie y cree el mapa
-#sleep 10
-
 # Iniciar nodo 6: Navegación (move_base, amcl, etc.)
-#gnome-terminal -- bash -c "roslaunch turtlebot3_navigation turtlebot3_navigation.launch; exec #bash"
+#gnome-terminal -- bash -c "roslaunch turtlebot3_navigation turtlebot3_navigation.launch #use_sim_time:=true open_rviz:=false; exec bash"
+
 
