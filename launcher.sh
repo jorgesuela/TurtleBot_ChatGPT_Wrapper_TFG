c81f752e6fda4281020cb9d0258f233c191154f5
jorgesuela <104921157+jorgesuela@users.noreply.github.com>
2025-04-05 15:13:50 +0200

    mapa de simulacion nuevo añadido

--- launcher.sh
+++ launcher.sh
@@ -6,22 +6,14 @@ gnome-terminal -- bash -c "roscore; exec bash"
 # Esperar un poco para asegurar que roscore arranque
 sleep 3
 
-# Iniciar nodo 1: TelegramSpeechToTextNode
+# Iniciar nodo 1
 gnome-terminal -- bash -c "cd ~/catkin_ws/src/turtlebot_chatgpt_wrapper && rosrun turtlebot_chatgpt_wrapper TelegramSpeechToTextNode.py; exec bash"
 
-# Iniciar nodo 2: ChatGPTProcessorNode
+# Iniciar nodo 2
 gnome-terminal -- bash -c "rosrun turtlebot_chatgpt_wrapper ChatGPTProcessorNode.py; exec bash"
 
-# Iniciar nodo 3: TurtleBotControlNode (Tu nodo que contiene el código TurtleBotActions)
+# Iniciar nodo 3
 gnome-terminal -- bash -c "rosrun turtlebot_chatgpt_wrapper TurtleBotControlNode.py; exec bash"
 
-# Iniciar Gazebo (Simulador)
-gnome-terminal -- bash -c "roslaunch turtlebot3_gazebo turtlebot3_house.launch; exec bash"
-
-# Iniciar move_base para la navegación
-gnome-terminal -- bash -c "roslaunch turtlebot3_navigation move_base.launch; exec bash"
-
-# Iniciar gmapping (o cualquier otro sistema SLAM)
-gnome-terminal -- bash -c "roslaunch turtlebot3_slam turtlebot3_slam.launch; exec bash"
-
-
+# Iniciar nodo 4
+gnome-terminal -- bash -c "rosrun turtlesim turtlesim_node; exec bash"
