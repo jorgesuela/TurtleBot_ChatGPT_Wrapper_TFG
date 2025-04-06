10a3338c5aa46c1e8222f99d2c4ac4acd76e2607
jorgesuela <104921157+jorgesuela@users.noreply.github.com>
2025-04-06 12:52:35 +0200

    Launcher correcto

--- launcher.sh
+++ launcher.sh
@@ -6,14 +6,22 @@ gnome-terminal -- bash -c "roscore; exec bash"
 # Esperar un poco para asegurar que roscore arranque
 sleep 3
 
-# Iniciar nodo 1
+# Iniciar nodo 1: TelegramSpeechToTextNode
 gnome-terminal -- bash -c "cd ~/catkin_ws/src/turtlebot_chatgpt_wrapper && rosrun turtlebot_chatgpt_wrapper TelegramSpeechToTextNode.py; exec bash"
 
-# Iniciar nodo 2
+# Iniciar nodo 2: ChatGPTProcessorNode
 gnome-terminal -- bash -c "rosrun turtlebot_chatgpt_wrapper ChatGPTProcessorNode.py; exec bash"
 
-# Iniciar nodo 3
+# Iniciar nodo 3: TurtleBotControlNode (Tu nodo que contiene el código TurtleBotActions)
 gnome-terminal -- bash -c "rosrun turtlebot_chatgpt_wrapper TurtleBotControlNode.py; exec bash"
 
-# Iniciar nodo 4
-gnome-terminal -- bash -c "rosrun turtlesim turtlesim_node; exec bash"
+# Iniciar Gazebo (Simulador)
+gnome-terminal -- bash -c "roslaunch turtlebot3_gazebo turtlebot3_casa.launch; exec bash"
+
+# Iniciar move_base para la navegación
+gnome-terminal -- bash -c "roslaunch turtlebot3_navigation move_base.launch; exec bash"
+
+# Iniciar gmapping (o cualquier otro sistema SLAM)
+gnome-terminal -- bash -c "roslaunch turtlebot3_slam turtlebot3_slam.launch; exec bash"
+
+
