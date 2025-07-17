63c631f4a819c3ccba6f6d96ef1201590700a10e
jorgesuela <104921157+jorgesuela@users.noreply.github.com>
2025-07-17 11:57:53 +0200

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,27 +1,52 @@
 #!/bin/bash
 
-# Iniciar ROS master (si no está corriendo)
-gnome-terminal -- bash -c "roscore; exec bash"
+# Iniciar Gazebo (Simulador)
+#gnome-terminal -- bash -c "roslaunch turtlebot_gazebo turtlebot_world.launch; exec bash"
 
 # Esperar un poco para asegurar que roscore arranque
-sleep 5
+#sleep 8
 
-# Iniciar nodo 1: TelegramSpeechToTextNode
-gnome-terminal -- bash -c "cd ~/catkin_ws/src/turtlebot_chatgpt_wrapper && rosrun turtlebot_chatgpt_wrapper SpeakLinkNode.py; exec bash"
+# tu ip: ifconfig
 
-# Iniciar nodo 2: ChatGPTProcessorNode
+# hay que comprobar si las ip han cambiado (primero conectar a la misma wifi) para probar : ping 192.168.71.172
+
+# para conectar con el robot desde el portatil:
+
+# ssh turtlebot@192.168.71.172 (la ip puede cambiar)
+
+# para ver la camara : rosrun image_view image_view image:=/camera/rgb/image_raw
+
+# para controlar el robot con el teclado: roslaunch turtlebot_teleop keyboard_teleop.launch 
+
+# PARA HACER MAPAS
+
+# roslaunch turtlebot_navigation gmapping_demo.launch
+
+# PARA GUARDAR MAPAS
+
+# rosrun map_server map_saver -f /tmp/my_map
+
+# PARA USAR MAPAS
+
+# primero cargar mapa
+
+# export TURTLEBOT_MAP_FILE=/tmp/my_map.yaml 
+
+# lanzar amcl
+
+# roslaunch turtlebot_navigation amcl_demo.launch
+
+# roslaunch turtlebot_rviz_launchers view_navigation.launch --screen
+# FUNCIONA Iniciar nodo 1: TelegramSpeechToTextNode
+gnome-terminal -- bash -c "cd ~/catkin_ws/src/turtlebot_chatgpt_wrapper && rosrun turtlebot_chatgpt_wrapper TelegramSpeechToTextNode.py; exec bash"
+
+# FUNCIONA Iniciar nodo 2: ChatGPTProcessorNode
 gnome-terminal -- bash -c "rosrun turtlebot_chatgpt_wrapper ChatGPTProcessorNode.py; exec bash"
 
-# Iniciar nodo 3: TurtleBotControlNode (Tu nodo que contiene el código TurtleBotActions)
+# FUNCIONA Iniciar nodo 3: TurtleBotControlNode (Tu nodo que contiene el código TurtleBotActions)
 gnome-terminal -- bash -c "rosrun turtlebot_chatgpt_wrapper TurtleBotControlNode.py; exec bash"
 
-# Iniciar Gazebo (Simulador)
-gnome-terminal -- bash -c "roslaunch turtlebot3_gazebo turtlebot3_casa.launch; exec bash"
 
-# Lanza move_base
-gnome-terminal -- bash -c "roslaunch turtlebot3_navigation move_base.launch; exec bash"
 
-# Iniciar gmapping (o cualquier otro sistema SLAM)
-gnome-terminal -- bash -c "roslaunch turtlebot3_slam turtlebot3_slam.launch; exec bash"
 
 
