4d699cec748e688ee1c7970c4acfb312335953e4
jorgesuela <104921157+jorgesuela@users.noreply.github.com>
2026-05-06 17:32:10 +0200

    bug resuelto
    se usaba el nodo telegram del proyecto antiguo

--- launcher.sh
+++ launcher.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Iniciar Gazebo (Simulador)
-#gnome-terminal -- bash -c "roslaunch turtlebot_gazebo turtlebot_world.launch; exec bash"
+gnome-terminal -- bash -c "roslaunch turtlebot_gazebo turtlebot_world.launch; exec bash"
 
 # Esperar un poco para asegurar que roscore arranque
-#sleep 8
+sleep 5
 
 # tu ip: ifconfig
 
@@ -15,6 +15,8 @@
 # ssh turtlebot@192.168.71.172 (la ip puede cambiar)
 
 # para ver la camara : rosrun image_view image_view image:=/camera/rgb/image_raw
+
+# para lanzar la camara: roslaunch astra_launch astra.launch
 
 # para controlar el robot con el teclado: roslaunch turtlebot_teleop keyboard_teleop.launch 
 
@@ -38,15 +40,14 @@
 
 # roslaunch turtlebot_rviz_launchers view_navigation.launch --screen
 # FUNCIONA Iniciar nodo 1: TelegramSpeechToTextNode
-gnome-terminal -- bash -c "cd ~/catkin_ws/src/turtlebot_chatgpt_wrapper && rosrun turtlebot_chatgpt_wrapper TelegramSpeechToTextNode.py; exec bash"
+gnome-terminal -- bash -c "cd ~/catkin_ws/src/cisc_turtlebot_chatgpt_wrapper && rosrun cisc_turtlebot_chatgpt_wrapper TelegramSpeechToTextNode.py; exec bash"
+
+sleep 1
 
 # FUNCIONA Iniciar nodo 2: ChatGPTProcessorNode
-gnome-terminal -- bash -c "rosrun turtlebot_chatgpt_wrapper ChatGPTProcessorNode.py; exec bash"
+gnome-terminal -- bash -c "rosrun cisc_turtlebot_chatgpt_wrapper ChatGPTProcessorNode.py; exec bash"
 
-# FUNCIONA Iniciar nodo 3: TurtleBotControlNode (Tu nodo que contiene el código TurtleBotActions)
-gnome-terminal -- bash -c "rosrun turtlebot_chatgpt_wrapper TurtleBotControlNode.py; exec bash"
+sleep 1
 
-
-
-
-
+# Nodo Wall Follower (activo pero sin hacer nada)
+gnome-terminal -- bash -c "rosrun cisc_turtlebot_chatgpt_wrapper wall_follower_node.py; exec bash"
